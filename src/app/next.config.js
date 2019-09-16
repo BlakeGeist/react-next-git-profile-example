519c6571451ce6f7ee6a17fc22079c21dd26d47f
blakegeist <blakegeist@gmail.com>
2019-09-15 17:28:20 -0700

    update styles
    update styles

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,5 +1,9 @@
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
+  webpack: (config, { dev }) => {
+    config.module.rules.push({ test: /\.scss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] });
+    return config;
+  },
   cssModules: true,
   distDir: process.env.NODE_ENV === 'production' ? `../functions/public` : '.next',
 })
--- src/app/pages/about.js
+++ src/app/pages/about.js
@@ -4,30 +4,12 @@ import Header from '../components/features/Header';
 import axios from 'axios';
 import Main from '../layouts/Main';
 
-class Index extends React.Component {
-  static async getInitialProps ({ reduxStore, req }) {
-      return {}
-  }
+const About = () => {
+  return (
+    <Main>
+      <h1>About the github cards app</h1>
+    </Main>
+  )
 
-  componentDidMount () {
-    // DISPATCH ACTIONS HERE FROM `mapDispatchToProps`
-    // TO TICK THE CLOCK
-    //this.timer = setInterval(() => this.props.startClock(), 1000)
-  }
-
-  componentWillUnmount () {
-  }
-
-  render () {
-    return (
-      <Main>
-        <h1>About the github cards app</h1>
-      </Main>
-    )
-  }
 }
-const mapDispatchToProps = {  }
-export default connect(
-  null,
-  mapDispatchToProps
-)(Index)
+export default About
